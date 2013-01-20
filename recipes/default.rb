c72f0f6f81c1dae83bedcc2a3a121745961c1957
sawanoboly <sawanoboriyu@higanworks.com>
2013-01-20 23:00:56 +0900

    add author

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,6 +4,7 @@
 #
 # Copyright 2013, HiganWorks LLC
 #
-# All rights reserved - Do Not Redistribute
+# Author: sawanoboriyu@higanworks.com
+# Licence: MIT
 #
 ## This recipe is empty. To include libralies for other cookbooks
