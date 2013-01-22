8303211b2e316180062c07c25579ac35d895b754
sawanoboly <sawanoboriyu@higanworks.com>
2013-01-22 19:55:54 +0900

    bump 0.2.0

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email 'sawanoboriyu@higanworks.com'
 license          'MIT'
 description      'Chef patches for joyent smartos(smartmachine).'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
 supports          "smartos"
